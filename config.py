35f7b5b2560af772a508ebe0abc26b40f5b4e658
David <d.ikkes2010@gmail.com>
2020-06-06 16:53:30 +0300

    created config file for project

---
+++ config.py
@@ -0,0 +1,19 @@
+from os import environ, path
+from dotenv import load_dotenv
+
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, '.env'))
+
+
+class Config:
+    """Set Flask configuration from environment variables."""
+
+    FLASK_APP = 'wsgi.py'
+    FLASK_ENV = environ.get('FLASK_ENV')
+    SECRET_KEY = environ.get('SECRET_KEY')
+    JWT_SECRET_KEY = environ.get('JWT_SECRET_KEY')
+
+    # Flask-SQLAlchemy
+    SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_ECHO = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
